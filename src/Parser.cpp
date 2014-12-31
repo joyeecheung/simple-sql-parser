37e72eadf97eec42112bbf60b387f55c5ebb473a
Joyee Cheung <joyeec9h3@gmail.com>
2015-01-01 05:21:39 +0800

    implement new parsing table

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -16,13 +16,15 @@ Create Parser::create_stmt() {
     if (lookahead == CREATE) {
         // create_stmt ->  CREATE TABLE id L_PAREN decl_list
         //                 R_PAREN SEMICOLON
-        match(CREATE); match(TABLE);
+        match(CREATE);
+        match(TABLE);
         string table_id = id();
         match(L_PAREN);
         multimap<string, int> defaults;
         vector<vector<string> > keys;
         decl_list(defaults, keys);
-        match(R_PAREN); match(SEMICOLON);
+        match(R_PAREN);
+        match(SEMICOLON);
         return Create(table_id, defaults, keys);
     } else {
         throw ParseError("Syntax error");
@@ -30,7 +32,7 @@ Create Parser::create_stmt() {
 }
 
 Parser & Parser::decl_list(multimap<string, int> &defaults, vector<vector<string> > &keys) {
-    if (lookahead == NUM || lookahead == ID) {
+    if (lookahead == ID || lookahead == PRIMARY) {
         // decl_list -> decl _decl_list
         decl(defaults, keys);
         _decl_list(defaults, keys);
@@ -43,9 +45,10 @@ Parser & Parser::decl_list(multimap<string, int> &defaults, vector<vector<string
 Parser & Parser::_decl_list(multimap<string, int> &defaults, vector<vector<string> > &keys) {
     if (lookahead == COMMA) {
         // _decl_list -> COMMA decl _decl_list
-        match(COMMA); decl(defaults, keys);
+        match(COMMA);
+        decl(defaults, keys);
         _decl_list(defaults, keys);
-    } else if (lookahead == R_PAREN || lookahead == END) {
+    } else if (lookahead == R_PAREN) {
         ; // _decl_list -> epsilon
     } else {
         throw ParseError("Syntax error");
@@ -62,7 +65,9 @@ Parser & Parser::decl(multimap<string, int> &defaults, vector<vector<string> > &
         defaults.insert(std::pair<string,int>(name, number));
     } else if (lookahead == PRIMARY) {
         // decl -> PRIMARY KEY L_PAREN column_list R_PAREN
-        match(PRIMARY); match(KEY); match(L_PAREN);
+        match(PRIMARY);
+        match(KEY);
+        match(L_PAREN);
         vector<string> columns;
         column_list(columns);
         keys.push_back(columns);
@@ -75,10 +80,12 @@ Parser & Parser::decl(multimap<string, int> &defaults, vector<vector<string> > &
 
 int Parser::default_spec() {
     if (lookahead == DEFAULT) {
-        // default_spec -> DEFAULT ASSIGN num
-        match(DEFAULT); match(ASSIGN);
-        return num();
-    } else if (lookahead == COMMA || lookahead == R_PAREN || lookahead == END) {
+        // default_spec -> DEFAULT ASSIGN expr[true]
+        match(DEFAULT);
+        match(ASSIGN);
+        Expr spec = expr(true);
+        return spec.eval();
+    } else if (lookahead == COMMA || lookahead == R_PAREN) {
         // default_spec -> epsilon
         return 0;  // if no default, default to zero
     } else {
@@ -103,7 +110,7 @@ Parser & Parser::_column_list(vector<string> &names) {
         match(COMMA);
         names.push_back(id());
         _column_list(names);
-    } else if (lookahead == FROM || lookahead == R_PAREN || lookahead == END){
+    } else if (lookahead == FROM || lookahead == R_PAREN){
         ;  // _column_list ->  epsilon
     } else {
         throw ParseError("Syntax error");
@@ -115,7 +122,8 @@ Insert Parser::insert_stmt() {
     if (lookahead == INSERT) {
         // insert_stmt -> INSERT INTO id L_PAREN column_list R_PAREN
         //                VALUES L_PAREN value_list R_PAREN SEMICOLON
-        match(INSERT); match(INTO);
+        match(INSERT);
+        match(INTO);
         string table_id = id();
         match(L_PAREN);
         vector<string> columns;
@@ -134,9 +142,11 @@ Insert Parser::insert_stmt() {
 }
 
 Parser & Parser::value_list(vector<int> &values) {
-    if (lookahead == NUM) {
-        // value_list ->  num _value_list
-        values.push_back(num());
+    if (lookahead == PLUS || lookahead == MINUS
+        || lookahead == NUM || lookahead == L_PAREN) {
+        // value_list -> expr[true] _value_list
+        Expr value = expr(true);
+        values.push_back(value.eval());
         _value_list(values);
     } else {
         throw ParseError("Syntax error");
@@ -146,11 +156,12 @@ Parser & Parser::value_list(vector<int> &values) {
 
 Parser & Parser::_value_list(vector<int> &values) {
     if (lookahead == COMMA) {
-        // _value_list ->  COMMA num _value_list
+        // _value_list -> COMMA expr[true] _value_list
         match(COMMA);
-        values.push_back(num());
+        Expr value = expr(true);
+        values.push_back(value.eval());
         _value_list(values);
-    } else if (lookahead == R_PAREN || lookahead == END){
+    } else if (lookahead == R_PAREN){
         ;  // _value_list ->  epsilon
     } else {
         throw ParseError("Syntax error");
@@ -161,7 +172,8 @@ Parser & Parser::_value_list(vector<int> &values) {
 Delete Parser::delete_stmt() {
     if (lookahead == DELETE) {
         // delete_stmt -> DELETE FROM id where_clause SEMICOLON
-        match(DELETE); match(FROM);
+        match(DELETE);
+        match(FROM);
         string table_id = id();
         Expr where = where_clause();
         match(SEMICOLON);
@@ -173,21 +185,23 @@ Delete Parser::delete_stmt() {
 
 Expr Parser::where_clause() {
     if (lookahead == WHERE) {
-        // where_clause -> WHERE conjunct_list
+        // where_clause -> WHERE disjunct
         match(WHERE);
-        return conjunct_list();
-    } else if (lookahead == SEMICOLON || lookahead == END) {
+        return disjunct();
+    } else if (lookahead == SEMICOLON) {
         return NULL_EXPR;  // where_clause -> epsilon
     } else {
         throw ParseError("Syntax error");
     }
 }
 
-Expr Parser::conjunct_list() {
-    if (lookahead == NUM || lookahead == ID) {
-        // conjunct_list -> bool _conjunct_list
-        Expr temp = boolean();
-        Expr test = _conjunct_list();
+Expr Parser::disjunct() {
+    if (lookahead == L_PAREN || lookahead == NOT
+        || lookahead == PLUS || lookahead == MINUS
+        || lookahead == NUM || lookahead == ID) {
+        // disjunct -> conjunct _disjunct
+        Expr temp = conjunct();
+        Expr test = _disjunct();
         if (test.isNull()) {  // test lack a left node
             return temp;
         } else {
@@ -199,14 +213,15 @@ Expr Parser::conjunct_list() {
     }
 }
 
-Expr Parser::_conjunct_list() {
-    if (lookahead == AND) {
-        // _conjunct_list -> AND bool _conjunct_list
-        match(AND);
-        Expr root(AND);
 
-        Expr temp = boolean();
-        Expr test = _conjunct_list();
+Expr Parser::_disjunct() {
+    if (lookahead == OR) {
+        // _disjunct -> OR conjunct _disjunct
+        match(OR);
+        Expr root(OR);
+
+        Expr temp = conjunct();
+        Expr test = _disjunct();
         if (test.isNull()) {
             root.setRight(temp);
             return root;
@@ -215,35 +230,212 @@ Expr Parser::_conjunct_list() {
             root.setRight(test);
             return root;
         }
-    } else if (lookahead == SEMICOLON || lookahead == END) {
-        return NULL_EXPR;
+    } else if (lookahead == SEMICOLON || lookahead == R_PAREN) {
+        return NULL_EXPR;  // _disjunct-> epsilon
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+
+Expr Parser::conjunct() {
+    if (lookahead == L_PAREN || lookahead == NOT
+        || lookahead == PLUS || lookahead == MINUS
+        || lookahead == NUM || lookahead == ID) {
+        // conjunct -> bool _conjunct_list
+        Expr temp = boolean();
+        Expr test = _conjunct();
+        if (test.isNull()) {  // test lack a left node
+            return temp;
+        } else {
+            test.setLeft(temp);
+            return test;
+        }
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+Expr Parser::_conjunct() {
+    if (lookahead == AND) {
+        // _conjunct -> AND bool _conjunct
+        match(AND);
+        Expr root(AND);
+
+        Expr temp = boolean();
+        Expr test = _conjunct();
+        if (test.isNull()) {
+            root.setRight(temp);
+            return root;
+        } else {
+            test.setLeft(temp);
+            root.setRight(test);
+            return root;
+        }
+    } else if (lookahead == OR || lookahead == SEMICOLON
+            || lookahead == R_PAREN) {
+        return NULL_EXPR;  // _conjunct -> epsilon
     } else {
         throw ParseError("Syntax error");
     }
 }
 
 Expr Parser::boolean() {
-    if (lookahead == NUM || lookahead == ID) {
-        Expr temp;
-        temp.setLeft(operand());
-        temp.setType(rop());
-        temp.setRight(operand());
+    if (lookahead == NUM || lookahead == ID
+     || lookahead == PLUS || lookahead == MINUS) {
+        return comp();
+    } else if (lookahead == L_PAREN) {
+        match(L_PAREN);
+        Expr temp = disjunct();
+        match(R_PAREN);
+        return temp;
+    } else if (lookahead == NOT) {
+        match(NOT);
+        Expr temp(NOT);
+        temp.setRight(disjunct());
         return temp;
     } else {
         throw ParseError("Syntax error");
     }
 }
 
-Expr Parser::operand() {
-    if (lookahead == NUM) {
+
+Expr Parser::comp() {
+    if (lookahead == NUM || lookahead == ID
+     || lookahead == PLUS || lookahead == MINUS) {
+        Expr temp;
+        temp.setLeft(expr(false));
+        temp.setType(rop());
+        temp.setRight(expr(false));
+        return temp;
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+Expr Parser::expr(bool simple) {
+    if (lookahead == NUM || lookahead == PLUS || lookahead == MINUS
+        || (simple && lookahead == L_PAREN) || (!simple && lookahead == ID)) {
+        // expr[] -> term[] _expr[]
+        Expr temp = term(simple);
+        Expr test = _expr(simple);
+        if (test.isNull()) {  // test lack a left node
+            return temp;
+        } else {
+            test.setLeft(temp);
+            return test;
+        }
+        return temp;
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+Expr Parser::_expr(bool simple) {
+    if (lookahead == PLUS || lookahead == MINUS) {
+        // _expr[] -> PLUS term[] _expr[]
+        // _expr[] -> MINUS term[] _expr[]
+        Type op = lookahead.getType();
+        match(op);
+        Expr root(op);
+
+        Expr temp = term(simple);
+        Expr test = _expr(simple);
+        if (test.isNull()) {
+            root.setRight(temp);
+            return root;
+        } else {
+            test.setLeft(temp);
+            root.setRight(test);
+            return root;
+        }
+    } else if (simple && (lookahead == COMMA || lookahead == R_PAREN)) {
+        return NULL_EXPR;  // _expr[] -> epsilon
+    } else if (!simple && (lookahead == NEQ || lookahead == EQ
+        || lookahead == LT || lookahead == GT || lookahead == LEQ
+        || lookahead == GEQ || lookahead == AND || lookahead == OR
+        || lookahead == SEMICOLON || lookahead == R_PAREN)) {
+        return NULL_EXPR;  // _expr[] -> epsilon
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+Expr Parser::term(bool simple) {
+    if (lookahead == NUM || lookahead == PLUS || lookahead == MINUS
+        || (simple && lookahead == L_PAREN) || (!simple && lookahead == ID)) {
+        // term[] -> unary[] _term[]
+        Expr temp = unary(simple);
+        Expr test = _term(simple);
+        if (test.isNull()) {  // test lack a left node
+            return temp;
+        } else {
+            test.setLeft(temp);
+            return test;
+        }
+        return temp;
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+
+Expr Parser::_term(bool simple) {
+    if (lookahead == MUL || lookahead == DIV) {
+        // _term[] -> MUL unary[] _term[]
+        // _term[] -> DIV unary[] _term[]
+        Type op = lookahead.getType();
+        match(op);
+        Expr root(op);
+
+        Expr temp = term(simple);
+        Expr test = _expr(simple);
+        if (test.isNull()) {
+            root.setRight(temp);
+            return root;
+        } else {
+            test.setLeft(temp);
+            root.setRight(test);
+            return root;
+        }
+    } else if (simple && (lookahead == COMMA || lookahead == R_PAREN)) {
+        return NULL_EXPR;  // _term[] -> epsilon
+    } else if (!simple && (lookahead == NEQ || lookahead == EQ
+        || lookahead == LT || lookahead == GT || lookahead == LEQ
+        || lookahead == GEQ || lookahead == AND || lookahead == OR
+        || lookahead == SEMICOLON || lookahead == R_PAREN)) {
+        return NULL_EXPR;  // _term[]-> epsilon
+    } else {
+        throw ParseError("Syntax error");
+    }
+}
+
+Expr Parser::unary(bool simple) {
+    if (lookahead == PLUS || lookahead == MINUS) {
+        // unary[] -> PLUS unary[]
+        // unary[] -> MINUS unary[]
+        Type op = lookahead.getType();
+        match(op);
+        Expr root(op);
+        root.setRight(unary(simple));
+        return root;
+    } else if (lookahead == NUM) {
+        // unary[] ->  num
         int result = num();
         Expr temp(NUM);
         temp.setValue(Token(NUM, &result, sizeof(result)));
         return temp;
-    } else if (lookahead == ID) {
+    } else if (!simple && lookahead == ID) {
+        // unary[false] -> id
         string result = id();
         Expr temp(ID);
         temp.setValue(Token(ID, result.c_str(), result.size()));
+        return temp;
+    else if (simple && lookahead == L_PAREN) {
+        // unary[true] -> L_PAREN expr[true] R_PAREN
+        match(L_PAREN);
+        Expr temp = expr(true);
+        match(R_PAREN);
         return temp;
     } else {
         throw ParseError("Syntax error");
@@ -265,8 +457,8 @@ Type Parser::rop() {
 Query Parser::query_stmt() {
     if (lookahead == SELECT) {
         // query_stmt -> SELECT select_list FROM id where_clause SEMICOLON
-        vector<string> columns;
         match(SELECT);
+        vector<string> columns;
         select_list(columns);
         match(FROM);
         string table_id = id();
