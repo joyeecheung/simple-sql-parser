28e69acad71db361bd7c152a712fd030a9884d48
joyeec9h3 <joyeec9h3@gmail.com>
2015-01-02 03:04:42 +0800

    draft for engine

--- draft/engine.cpp
+++ draft/engine.cpp
@@ -8,12 +8,14 @@ private:
     string msg;
 };
 
+#define MAX_COL 100
+#define MAX_KEY 100
 
-struct Column {
+struct Scheme {
     string name;
     int def;
     bool is_key;
-    Column(string _name, int def, bool _is_key)
+    Scheme(string _name, int def, bool _is_key)
         : name(_name), def(_def), is_key(_is_key) {}
 };
 
@@ -57,11 +59,17 @@ int main(int argc, char const *argv[]) {
                 cout << delete_stmt.getId() << "\n";
             } else if (next == SELECT) {
                 Query query_stmt = parser.query_stmt();
+                string table_id = query_stmt.getId();
 
                 vector<vector<int> > results;
-                vector<string> names;
-                int number = engine.query(stmt, names, results);
+                vector<string> names = query_stmt.getColumns();
+
+                int number = engine.query(query_stmt, results);
                 if (number > 0) {
+                    if (std::find(name.begin(), names.end(), "*") != names.end()) {
+                        names = engine.getColumns(table_id);
+                    }
+
                     for (auto name : names) {
                         cout << name << '\t';
                     }
@@ -93,209 +101,272 @@ int main(int argc, char const *argv[]) {
 
 class Engine {
 public:
-    bool create(const Statement &stmt) {
-        const Create &create_stmt = dynamic_cast<const Create &>(stmt);
-        if (create_stmt == NULL) {
-            return false;
-        }
-
+    bool create(const Create &create_stmt) {
+        // check the table is not created before
         string table_id = create_stmt.getId();
         if (tables.find(table_id) != tables.end()) {
-            throw DataBaseError(string("Table") + table_id + string("already exists"));
+            throw DataBaseError(string("Table ") + table_id + string(" already exists"));
         }
 
-        Table new_table = Table(table_id, create_stmt.getDefaults(),
-                                create_stmt.getKeys());
+        // check no multiple primary keys
+        const vector<vector<string> > &keys = create.getKeys();
+        if (keys.size() > 1) {
+            throw DataBaseError("Multiple primary key definitions"));
+        }
+
+        // check no duplicate column definitions
+        const multimap<string, int> &defs = create.getDefaults();
+        map<string, int> unique_defs;
+        for(auto it = defs.begin(); it != defs.end(); it = defs.upper_bound(it->first)) {
+            if (defaults.count(it->first) > 1) {
+               throw DataBaseError(string("Multiple definitions for ") + it->first);
+            } else {
+                unique_defs[it->first] = it->second;
+            }
+        }
+
+        // check all primary keys have definitions
+        const vector<string> &primary = keys[0];
+        for (auto key: primary) {
+            if (defs.find(key) == defs.end()) {
+                throw DataBaseError(string("Undefined key ") + key);
+            }
+        }
+
+        if (unique_defs.size() > MAX_COL) {
+            throw DataBaseError(string("Number of columns should be no more than ")
+                                + to_string(MAX_COL));
+        }
+
+        if (primary.size() > MAX_KEY) {
+            throw DataBaseError(string("Number of keys should be no more than ")
+                                + to_string(MAX_KEY));
+        }
+
+        Table new_table = Table(table_id, unique_defs, primary);
         tables[create_stmt.id] = new_table;
         return true;
     }
 
     // insert the values
-    bool insert(const Statement &stmt) {
-        const Insert &insert_stmt = dynamic_cast<const Insert &>(stmt);
-        if (insert_stmt == NULL) {
-            return false;
-        }
-
+    bool insert(const Insert &insert_stmt) {
+        // the table should exist
         string table_id = insert_stmt.getId();
         auto it = tables.find(table_id);
         if (it == tables.end()) {
-            throw DataBaseError(string("Cannot find table") + table_id);
+            throw DataBaseError(string("Cannot find table ") + table_id);
         }
 
-        it->insert(insert_stmt.getColumns(), insert_stmt.getValues());
-        return true;
+        set<string> checked;
+        const vector<string> &columns = insert_stmt.getColumns();
+        const vector<int> &values = insert_stmt.getValues();
+
+        // # of columns should equal to # of values
+        if (columns.size() != values.size()) {
+            throw DataBaseError("Numbers of columns and values do not match");
+        }
+
+        // no duplicate columns
+        for (auto col : columns) {
+            if (checked.find(col) != checked.end()) {
+                throw DataBaseError(string("Duplicate column ") + col);
+            } else {
+                checked.insert(col);
+            }
+        }
+
+        // no primary key constraint violation(all have/already in)
+        // all columns should be in the schema of the table
+        return it->insert(columns, values);
     }
 
     // delete the records
-    bool del(const Statement &stmt) {
-        const Delete &delete_stmt = dynamic_cast<const Delete &>(stmt);
-        if (delete_stmt == NULL) {
-            return false;
-        }
-
+    int del(const Delete &delete_stmt) {
+        // the table should exist
         string table_id = delete_stmt.getId();
         auto it = tables.find(table_id);
         if (it == tables.end()) {
-            throw DataBaseError(string("Cannot find table") + table_id);
+            throw DataBaseError(string("Cannot find table ") + table_id);
         }
 
-        it->del(delete_stmt.getWhere());
-        return true;
+        // columns occurring in the where clause (if any)
+        // should be in the schema of the table
+        return it->del(delete_stmt.getWhere());
     }
 
     // query the records
-    Engine &query(const Statement &stmt, vector<strings> &names,
-                  vector<vector<int> > &results) const {
-        const Query &query_stmt = dynamic_cast<const Query &>(stmt);
-        if (query_stmt == NULL) {
-            return false;
-        }
-
+    int query(const Insert &query_stmt, vector<vector<int> > &results) const {
+        // the table should exist
         string table_id = query_stmt.getId();
         auto it = tables.find(table_id);
         if (it == tables.end()) {
-            throw DataBaseError(string("Cannot find table") + table_id);
+            throw DataBaseError(string("Cannot find table ") + table_id);
         }
 
-        it->query(names, query_stmt.getWhere(), results);
-        return bool;
+        // all columns (except *) in the select list should be in
+        // the schema of the table
+        // columns occurring in the where clause (if any)
+        // should be in the schema of the table
+        return it->query(query_stmt.getColumns(), query_stmt.getWhere(), results);
     }
 
-    vector<string> &getColumnNames(string table_id) {
+    const vector<string> &getColumns(string table_id) {
         auto it = tables.find(table_id);
         if (it == tables.end()) {
-            throw DataBaseError(string("Cannot find table") + table_id);
+            throw DataBaseError(string("Cannot find table ") + table_id);
         }
 
-        return it->getColumnNames();
+        return it->getColumns();
     }
 
 private:
-    map<id, Table> tables;
+    map<string, Table> tables;
 };
 
 class Table {
 public:
     Table(const string table_id, const map<string, int> defs,
-          const vector<string> _strkeys)
-        : id(table_id), strkeys(_strkeys) {
+          const vector<string> primary)
+        : id(table_id), keys(primary.begin(), primary.end()) {
         int counter = 0;
         for (auto it = defs.begin(); it != defs.end(); ++it) {
-            Column new_column = Column(it->first, it->second; counter, false);
-            indexes[it->first] = counter;
-            columns.push_back(new_column);
-            columnNames.push_back(it->first);
+            Scheme new_scheme = Scheme(it->first, it->second,
+                                       keys.find(it->first) != keys.end());
+            indexes[it->first] = counter++;
+            schema.push_back(new_scheme);
+            columns.push_back(it->first);
         }
+
     }
 
     Table &insert(const vector<string> cols, const vector<int> values) {
-        if (columns.size() < values.size()) {
-            throw DataBaseError(string("Too many values");
-        } else if (columns.size() == values.size()) {
-            // check no missed key
-            for (auto strkey : strkeys) {
-                if (std::find(cols.begin(), cols.end(), strkey) == cols.end()) {
-                    throw DataBaseError(string("Key ") + strkey + string(" not found"));
-                }
-            }
 
-            // check not already exists
-            for (auto record : data) {
-                if (conflict(record, cols, values)) {
-                    throw DataBaseError(string("Record already exists"));
-                }
+        // check no primary key constraint violation
+        set<string> colset(cols.begin(), cols.end());
+        for (auto key : keys) {
+            if (colset.find(key) == colset.end()) {
+                throw DataBaseError(string("Key ") + key + string(" not found"));
             }
-
-            // fill in defaults
-            vector<int> new_record(columns.size());
-            // fill in default data
-            for (int i = 0; i < columns.size(); ++i) {
-                new_record[i] = columns[i].def;
-            }
-
-            // fill in available data
-            for (int i = 0; i < cols.size(); ++i) {
-                int index = indexes[col[i]];
-                new_record[index] = values[i];
-            }
-
         }
+
+        // all columns should be in the schema of the table
+        for(col : colset) {
+            if (std::find(columns.begin(), columns.end(), col) == columns.end()) {
+                throw DataBaseError(string("Column ") + col + string(" is not in the schema"));
+            }
+        }
+
+        vector<int> new_record(columns.size());
+        // fill in default data
+        for (int i = 0; i < columns.size(); ++i) {
+            new_record[i] = schema[i].def;
+        }
+
+        // fill in available data
+        for (int i = 0; i < cols.size(); ++i) {
+            int index = indexes[cols[i]];
+            new_record[index] = values[i];
+        }
+
+        // check not already exists
+        for (auto record : data) {
+            if (conflict(record, new_record)) {
+                throw DataBaseError(string("Record already exists"));
+            }
+        }
+
+        data.push_back(new_record);
     }
 
-    Table &del(const Expr expr) {
-        // match the expression against each record
-        auto it = data.begin()
+    int del(const Expr expr) {
+        auto it = data.begin();
+        int count = 0;
+
+        // columns occurring in the where clause (if any)
+        // should be in the schema of the table
         while (it != data.end()) {
             if (expr.eval(*it, indexes)) {
                 data.erase(it++);
+                count++;
             } else {
                 it++;
             }
         }
+
+        return count;
     }
 
-    Table &query(vector<string> &names, const Expr expr,
+    int query(const vector<string> &names, const Expr expr,
                  vector<vector<int> > &results) const {
-        vector<string> query_columns = query_stmt.getColumns();
-        if (std::find(query_columns.begin(), query_columns.end(), '*')) {
-            std::copy(columnNames.begin(), columnNames.end(), names.begin());
-        } else {
-            std::copy(query_columns.begin(), query_columns.end(), names.begin());
-        }
+        int count = 0;
 
-        vector<int> query_indexes;
-        for (auto name : names) {
-            query_indexes.push_back(indexes[name]);
-        }
-
-        auto it = data.begin()
-        while (it != data.end()) {
-            const vector<int> &record = *it;
-            if (expr.eval(record, indexes)) {
-                vector<int> reordered;
-                for (index : query_indexes) {
-                    reordered.push_back(record[index]);
+        // get queried columns
+        if (std::find(names.begin(), names.end(), "*")) { // all
+            for (auto record : data) {
+                if (expr.eval(record, indexes)) {
+                    // columns occurring in the where clause (if any)
+                    // should be in the schema of the table
+                    results.push_back(record);
+                    count++;
                 }
-
-                results.push_back(reordered);
-                it++;
-            } else {
-                it++;
             }
+            return count;
+        } else {  // selected columns
+            // all columns (except *) in the select list should be in
+            // the schema of the table
+            for(name : names) {
+                if (std::find(columns.begin(), columns.end(), name) == columns.end()) {
+                    throw DataBaseError(string("Column ") + name + string(" is not in the schema"));
+                }
+            }
+
+            // use int index for reordering
+            vector<int> query_indexes;
+            for (auto name : names) {
+                query_indexes.push_back(indexes[name]);
+            }
+
+            for (auto record : data) {
+                if (expr.eval(record, indexes)) {
+                    // columns occurring in the where clause (if any)
+                    // should be in the schema of the table
+                    vector<int> reordered;
+                    for (idx : query_indexes) {
+                        reordered.push_back(record[idx]);
+                    }
+                    results.push_back(reordered);
+                    count++;
+                }
+            }
+
+            return count;
         }
 
     }
 
-    vector<string> getKeys() const {
-        return strkeys;
+    const vector<string> &getColumns() const {
+        return columns;
     }
 
-    const vector<string> &getColumnNames() const {
-        return columnNames;
-    }
-
-    bool conflict(const vector<int> record, const vector<string> cols,
-                  const vector<int> values) const {
-        for (int i = 0; i < cols.size(); ++i) {
-            int index = indexes[cols[i]];
-            if (columns[index].is_key) {
-                int value = values[i];
-                if (record[index] != value) {
-                    return false;
-                }
+    bool conflict(const vector<int> old_record, const vector<int> new_record) const {
+        for (key: keys) {
+            int idx = indexes[key];
+            if (old_record[idx] != new_record[idx]) {
+                return false;
             }
         }
         return true;
     }
 
+
 private:
     string id;
 
-    vector<string> strkeys;
-
-    vector<vector<int> > data;
+    set<string> keys;
     map<string, int> indexes;
-    vector<Column> columns;
-    vector<string> columnNames;
+
+    // same order
+    vector<Scheme> schema;
+    vector<string> columns;
+    vector<vector<int> > data;
 };
